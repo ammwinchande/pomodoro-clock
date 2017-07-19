00e0c1128407887272c525fec98e4ac10f172c2e
ammwinchande <mohammad.chandey@gmail.com>
2017-07-19 16:54:53 +0300

    add display function

---
+++ pomodoro_clock.py
@@ -0,0 +1,17 @@
+
+
+def display():
+    """
+    Display style for the program
+    :return:
+    """
+    print("\n*****************************************")
+    print("*\t\t\t Pomodoro Clock \t\t\t*")
+    print("*\t\t author: m.a. mwinchande \t\t*")
+    print("*****************************************")
+    print("\n")
+
+
+# execute the program
+display()
+
